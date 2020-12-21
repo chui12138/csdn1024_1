01e3f9ff877efc8f53b464e0e1b9df348db9ad93
chui12138 <zwx1589837@foxmail.com>
2020-12-21 01:03:11 -0800

    final

--- after/main.c
+++ after/main.c
@@ -28,7 +28,96 @@ void dysj(int d)//打印等腰三角形
 		n();
 	}
 }
+void jx(int a,int b)//空心矩形比较简单
+{
+	pline(a,'*');
+	n();
+	for(int i=0;i<b-2;i++)
+	{
+		pline(1,'*');
+		pline(a-2,' ');//实心矩形只要改成‘*’就可以
+		pline(1,'*');
+		n();
+	}
+	pline(a,'*');
+}
+void hui(int a,int b)
+//回型两个空心正方形组合,a是外部正方形大小，b是内部大小,默认是奇数
+{
+	pline(a,'*');
+	n();
+	for(int i=0;i<(a-b-2)/2;i++)
+	{
+		pline(1,'*');
+		pline(a-2,' ');
+		pline(1,'*');
+		n();
+	}
+	pline(1,'*');
+	pline((a-b-2)/2,' ');
+	pline(b,'*');
+	pline((a-b-2)/2,' ');
+	pline(1,'*');
+	n();
+	for(int i=0;i<(b-2);i++)
+	{
+		pline(1,'*');
+		pline((a-b-2)/2,' ');
+		pline(1,'*');
+		pline(b-2,' ');
+		pline(1,'*');
+		pline((a-b-2)/2,' ');
+		pline(1,'*');
+		n();
+	}
+
+	pline(1,'*');
+	pline((a-b-2)/2,' ');
+	pline(b,'*');
+	pline((a-b-2)/2,' ');
+	pline(1,'*');
+	n();
+	
+	for(int i=0;i<(a-b-2)/2;i++)
+	{
+		pline(1,'*');
+		pline(a-2,' ');
+		pline(1,'*');
+		n();
+	}
+	
+	pline(a,'*');
+}
+void lingx(int a)//菱形对角线一半长度为a
+{
+	/*前两行打印方法,类似打印两个等腰三角形拼在一起
+	pline(a,' ');
+	pline(1,'*');
+	n();
+	pline(a-1,' ');
+	pline(3,'*');
+	n();*/
+	for(int i=0;i<a;i++)
+	{
+		pline(a-i,' ');
+		pline(1+i*2,'*');
+		n();
+	}
+	for(int i=1;i<a;i++)
+	{
+		pline(1+i,' ');
+		pline(1+(a-i-1)*2,'*');
+		n();
+	}
+}
 int main()
 {
 	dysj(6);
+	n();
+	jx(8,4);
+	n();
+	hui(11,7);
+	n();
+	lingx(8);
+	return 0;
 }
